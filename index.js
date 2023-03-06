e82054b234fcdae92e48c41e4589675f7626fbed
Jai <jaijai.stell@gmail.com>
2023-03-06 16:01:42 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+//was Joe
+const height = 39;
+//was 74
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
